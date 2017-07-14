f100caa432b91e038d851914c985d748ce6f484c
NoamAvisrur <noamavisrur@gmail.com>
2017-07-14 17:56:07 +0300

    added some style

--- js/config_routes.js
+++ js/config_routes.js
@@ -12,7 +12,7 @@ app.config(function($routeProvider) {
                                 <input type="radio"  ng-model="q.value" value="Andromeda" ng-change='q.getValue()'> Andromeda<br>
                                 <input type="radio"  ng-model="q.value" value="Milky Way" ng-change='q.getValue()'> Milky Way<br>  
                             </div>
-                        <a href="#!2" class="next_button">next</a>
+                        <a href="#!2" class="first_next_button">next  &#9658;</a>
                     </div>`,
         controller : "q1"
     })
@@ -27,8 +27,10 @@ app.config(function($routeProvider) {
                             <input type="radio" ng-model="q.value" value="859,000km" ng-change='q.getValue()'> 859,000km<br>
                             <input type="radio" ng-model="q.value" value="384,400km" ng-change='q.getValue()'> 384,400km<br>
                             <input type="radio" ng-model="q.value" value="453,000km" ng-change='q.getValue()'> 453,000km<br> 
-                        </div>    
-                        <a href="#!3" class="next_button">next</a>
+                        </div>  
+                        <div class="nav">
+                            <a href="#!1" class="next_button">&#9668;  previous</a><a href="#!3" class="next_button">next  &#9658;</a>
+                         </div>   
                     </div>`,
         controller : "q2"
     })
@@ -44,7 +46,9 @@ app.config(function($routeProvider) {
                             <input type="radio" ng-model="q.value" value="mars" ng-change='q.getValue()'> Mars<br>
                             <input type="radio" ng-model="q.value" value="venus" ng-change='q.getValue()'> Venus<br>
                         </div>    
-                        <a href="#!4" class="next_button">next</a></div>
+                        <div class="nav">                
+                            <a href="#!2" class="next_button">&#9668;  previous</a><a href="#!4" class="next_button">next  &#9658;</a>
+                        </div>
                     </div>`,
        controller : "q3"
     })
@@ -59,8 +63,10 @@ app.config(function($routeProvider) {
                             <input type="radio" ng-model="q.value" value="5,500 degrees C" ng-change='q.getValue()'> 5,500 degrees C<br>
                             <input type="radio" ng-model="q.value" value="10,500 degrees C" ng-change='q.getValue()'> 10,500 degrees C<br>
                             <input type="radio" ng-model="q.value" value="12,500 degrees C" ng-change='q.getValue()'> 12,500 degrees C<br>
-                       </div>     
-                       <a href="#!5" class="next_button">next</a></div>
+                       </div>  
+                        <div class="nav">               
+                            <a href="#!3" class="next_button">&#9668;  previous</a><a href="#!5" class="next_button">next  &#9658;</a>
+                       </div>
                     </div>`,
        controller : "q4"
     })
@@ -75,8 +81,11 @@ app.config(function($routeProvider) {
                             <input type="radio" ng-model="q.value" value="saturn" ng-change='q.getValue()'> Saturn<br>
                             <input type="radio" ng-model="q.value" value="jupiter" ng-change='q.getValue()'> Jupiter<br>
                             <input type="radio" ng-model="q.value" value="pluto" ng-change='q.getValue()'> Pluto<br>
+                        </div>
+                        <div class="nav">
+                            <a href="#!4" class="next_button">&#9668;  previous</a><button ng-click="ctrl.submit()" class="submit_button">submit</button>
                         </div>    
-                     <button ng-click="ctrl.submit()" class="submit_button">submit</button></div>`,
+                     </div>`,
        controller : "q5"
     })    
     .otherwise({
