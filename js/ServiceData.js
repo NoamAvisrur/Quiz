7f7b8c5bbe2cba57fc11325c9a5909bfc9b38a80
NoamAvisrur <noamavisrur@gmail.com>
2017-07-18 15:09:36 +0300

    rebuilt the quiz project

--- js/ServiceData.js
+++ js/ServiceData.js
@@ -1,32 +1,55 @@
 app.service('DataService', function() {
-    this.questions= [
-                      {Q1: "what's the name of our galaxy?"},
-                      {Q2: "what's the distance from the earth to the moon?"},
-                      {Q3: "which planet is closest to the sun?"},
-                      {Q4: "what's the temperature of the sun's surface?"},
-                      {Q4: "what's the biggest planet in the solar system?"}
+    this.data = [
+                      {
+                          Q: "what's the name of our galaxy?",
+                          A: "Milky Way",
+                          O: [
+                              "LMC1300",
+                              "Andromeda",
+                              "Milky Way"
+                              ]
+                      },
+                      {
+                          Q: "what's the distance from the earth to the moon?",
+                          A: "384,400km",
+                          O: [
+                              "859,000km",
+                              "384,400km",
+                              "453,000km",
+                              "295,050km"
+                          ]
+                      },
+                      {
+                          Q: "which planet is closest to the sun?",
+                          A: "Mercury",
+                          O: [
+                              "Mercury",
+                              "Earth",
+                              "Venus"
+                          ]
+                      },
+                      {
+                          Q: "what's the temperature of the sun's surface?",
+                          A: "5,500 degrees C",
+                          O: [
+                              "10,500 degrees C",
+                              "5,500 degrees C",
+                              "12,500 degrees C",
+                              "9,500 degrees C",
+                              "2,500 degrees C"
+                          ]
+                      },
+                      {
+                          Q: "what's the biggest planet in the solar system?",
+                          A: "Jupiter",
+                          O: [
+                              "Saturn",
+                              "Jupiter",
+                              "Neptun"
+                          ]
+                      }
                     ]
                     
     this.answers = [];
-    
-    this.rightAnswers = [];
-    
-    this.currectAnswers = ["Milky Way",
-                           "384,400km",
-                           "venus",
-                           "5,500 degrees C",
-                           "jupiter"]
 
-    this.calculate = function(){
-        var pointsPerQ = 100/this.answers.length;
-        this.score = 0;
-        for(var i = 0; i < this.answers.length; i++){
-             if(this.answers[i] === this.currectAnswers[i]){
-                 this.score += pointsPerQ;
-                 this.rightAnswers.push(this.answers[i]);
-             }     
-        }
-        console.log(this.score);
-        console.log(this.rightAnswers);
-    }
 })
--- js/main.js
+++ js/main.js
@@ -1,38 +1,71 @@
-var app = angular.module("myApp", ["ngRoute"]);
+var app = angular.module("myApp", []);
 
 app.controller('mainCtrl', function(DataService){
+    
     this.title = 'The Big Space Quiz';
-        
+    
+    this.data = DataService.data;
+    
     this.show = false;
     
-    this.checkArr = true;
-        
-    this.submit = function(){
-        this.checkArr = true;
-        for (var i = 0; i < DataService.answers.length; i++){
-            if(typeof DataService.answers[i] == 'undefined' || DataService.answers.length !== 5){
-                this.checkArr = false;
-                break;
-            }  
-        }
-        if(this.checkArr) {
-            if(document.querySelector('.notice')){document}   
-            DataService.calculate();
-            this.score = DataService.score;
-            this.show = true;
-            document.querySelector('[ng-view]').innerHTML = ""; 
-            this.answersLength = DataService.answers.length
-            this.currectAnswersLength = DataService.rightAnswers.length;
-        }else{
-            console.log('didnt finish');
-        } 
+    this.answers = []
+    
+    this.i = 0;
+    
+    this.nav = function(mode){
+        if(mode === 'next'){this.i += 1;};
+        if(mode === 'prev'){this.i -= 1};
     }
     
-    this.resetQuiz = function(){
-        DataService.answers = [];
-        DataService.rightAnswers = [];
-        this.show = false;
-        DataService.score = 0;
+    this.startQuiz = function(){
+        this.show = true;
+        document.querySelector('.msg').remove();
     }
+    
+    this.getValue = function($index){
+        DataService.answers[this.i] = this.answers[this.i];
+        console.log(DataService.answers);
+        console.log(this.answers);
+    }
+    
+    this.submit = function(){
+        this.calculate();
+        this.show = false;
+    }
+    
+    this.calculate = function(){
+        var pointsPerQ = 100/this.data.length;
+        console.log(pointsPerQ);
+        this.score = 0;
+         for (var i = 0; i < this.data.length; i++){
+             console.log (this.data[i].A);
+             console.log(DataService.answers[i]);
+             if(this.data[i].A === DataService.answers[i]){
+                 this.score += pointsPerQ;
+                 this.rightAnswers.push(DataService.answers[i]);
+             }     
+        }
+        console.log(this.score);
+        console.log(this.rightAnswers);
+    }
+    
+    this.rightAnswers = [];
+    
 })
 
+app.component('resultComponent', {
+  template: `<div class="showResults" ng-show="result.score">
+                <h1>results:</h1>
+                <span class="score">Your Score: <span>{{result.score | number:0}}</span></span>
+                <span>you answersed {{result.rightanswers.length}} out of {{result.answers.length}} currect answers</span>
+            </div> `,
+  bindings: {
+       score: "<",
+       rightanswers: "<",
+       answers: "<"
+  },
+    controller: function($element) {
+
+  },
+  controllerAs: 'result'
+});
